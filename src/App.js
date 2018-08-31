e6026c0db97618dc21cc4981e92fbcef460555c1
Alex Rosado <Ace@Macbooks-MacBook-Pro-4.local>
2018-08-31 14:39:12 -0400

    firebase configured

--- src/App.js
+++ src/App.js
@@ -3,8 +3,22 @@ import { View, Text } from 'react-native';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import reducers from './reducers';
+import firebase from 'firebase';
 
 class App extends Component {
+    componentWillMount() {
+        // Initialize Firebase
+  const config = {
+    apiKey: "AIzaSyAmMShlq5enfvuD4o2FM9dqrQdSSaZOwug",
+    authDomain: "manager-86f1e.firebaseapp.com",
+    databaseURL: "https://manager-86f1e.firebaseio.com",
+    projectId: "manager-86f1e",
+    storageBucket: "manager-86f1e.appspot.com",
+    messagingSenderId: "698351699843"
+  };
+  firebase.initializeApp(config);
+    }
+
     render() {
         return (
             <Provider store={createStore(reducers)}>

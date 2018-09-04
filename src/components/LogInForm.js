c4d3e11eefa3df6a97b0b0be6ecaf545ed181c56
Alex Rosado <Ace@Macbooks-MacBook-Pro-4.local>
2018-09-04 13:12:53 -0400

    update reducers

--- src/components/LogInForm.js
+++ src/components/LogInForm.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
 import { Card, CardSection, Input, Button } from './common';
 import { connect } from 'react-redux';
-import { emailChanged } from '../actions';
+import { emailChanged, passwordChanged } from '../actions';
 
 class LogInForm extends Component {
     onEmailChange(text) {
         this.props.emailChanged(text);
+    }
+
+    onPasswordChange(text) {
+        this.props.passwordChanged(text);
     }
 
     render() {
@@ -16,6 +20,7 @@ class LogInForm extends Component {
                     label="Email"
                     placeholder="email@gmail.com"
                     onChangeText={this.onEmailChange.bind(this)}
+                    value={this.props.email}
                     />
                 </CardSection>
                     
@@ -24,6 +29,8 @@ class LogInForm extends Component {
                     secureTextEntry
                     label="Password"
                     placeholder="password"
+                    onChangeText={this.onPasswordChange.bind(this)}
+                    value={this.props.password}
                     />
                 </CardSection>
 
@@ -37,4 +44,11 @@ class LogInForm extends Component {
     }
 }
 
-export default connect(null, { emailChanged })(LogInForm);
+const mapStateToProps = state => {
+    return {
+        email: state.auth.email,
+        password: state.auth.password
+    }
+};
+
+export default connect(mapStateToProps, { emailChanged, passwordChanged })(LogInForm);

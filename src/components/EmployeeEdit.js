278f83d6f5af39f9f9c60ca8c36f9c49ba35a188
Alex Rosado <Ace@Macbooks-MacBook-Pro-4.local>
2018-09-17 19:45:35 -0400

    edit employee success

--- src/components/EmployeeEdit.js
+++ src/components/EmployeeEdit.js
@@ -2,7 +2,7 @@ import _ from 'lodash';
 import React, { Component } from 'react';
 import EmployeeForm from './EmployeeForm'
 import { Card, CardSection, Button } from './common';
-import { employeeUpdate } from '../actions';
+import { employeeUpdate, employeeSave } from '../actions';
 import { connect } from 'react-redux';
 
 class EmployeeEdit extends Component {
@@ -14,6 +14,8 @@ class EmployeeEdit extends Component {
 
     onButtonPress() {
         const { name, phone, shift } = this.props;
+
+        this.props.employeeSave({ name, phone, shift, uid: this.props.employee.uid })
     }
     render() {
         return (
@@ -35,4 +37,4 @@ const mapStateToProps = (state) => {
     return { name, phone, shift };
 };
 
-export default connect(mapStateToProps, { employeeUpdate })(EmployeeEdit);
+export default connect(mapStateToProps, { employeeUpdate, employeeSave })(EmployeeEdit);

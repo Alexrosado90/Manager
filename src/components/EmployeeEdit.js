e1209aeaede95dc800e8bab548c6895b4fd7d399
Alex Rosado <Ace@Macbooks-MacBook-Pro-4.local>
2018-09-18 16:39:32 -0400

    Delete employee successfully

--- src/components/EmployeeEdit.js
+++ src/components/EmployeeEdit.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 import EmployeeForm from './EmployeeForm'
 import Communications from 'react-native-communications';
 import { Card, CardSection, Button, Confirm } from './common';
-import { employeeUpdate, employeeSave } from '../actions';
+import { employeeUpdate, employeeSave, employeeDelete } from '../actions';
 import { connect } from 'react-redux';
 
 class EmployeeEdit extends Component {
@@ -24,6 +24,16 @@ class EmployeeEdit extends Component {
     onTextPress() {
         const { phone, shift } = this.props;
         Communications.text(phone, `your upcoming shift is on ${shift}`)
+    }
+
+    onAccept() {
+        const { uid } = this.props.employee;
+
+        this.props.employeeDelete({ uid })
+    }
+
+    onDecline() {
+        this.setState({ showModal: false });
     }
 
     render() {
@@ -47,7 +57,10 @@ class EmployeeEdit extends Component {
                     Fire Employee
                 </Button>
             <Confirm
-            visible={this.state.showModal}>
+            visible={this.state.showModal}
+            onAccept={this.onAccept.bind(this)}
+            onDecline={this.onDecline.bind(this)}
+            >
                 Are you sure you want to fire?
             </Confirm>
             </CardSection>
@@ -62,4 +75,4 @@ const mapStateToProps = (state) => {
     return { name, phone, shift };
 };
 
-export default connect(mapStateToProps, { employeeUpdate, employeeSave })(EmployeeEdit);
+export default connect(mapStateToProps, { employeeUpdate, employeeSave, employeeDelete })(EmployeeEdit);
